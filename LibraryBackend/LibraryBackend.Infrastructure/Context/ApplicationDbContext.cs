075400416d5f70724d72cbc2f64eedb3ddd0f0bc
salvationlloydstanley-kemuel <skemuel008@gmail.com>
2023-09-08 15:49:23 +0100

    Create ApplicationDbContext class

---
+++ LibraryBackend/LibraryBackend.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using LibraryBackend.Core.Common;
+using LibraryBackend.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryBackend.Infrastructure.Context;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<AuditBaseEntity<Guid>>())
+        {
+            entry.Entity.UpdatedDate = DateTime.UtcNow;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedDate = DateTime.UtcNow;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
